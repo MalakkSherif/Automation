8711244f6707ebb10a4e3d9b6ad7151058a7ea93
MalakkSherif <malaksherifmohamed@gmail.com>
2025-08-13 16:31:49 +0300

    BlogPage class done

--- src/test/java/Base/TestBase.java
+++ src/test/java/Base/TestBase.java
@@ -1,4 +1,26 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
 public class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
--- src/test/java/Pages/BlogPage.java
+++ src/test/java/Pages/BlogPage.java
@@ -1,4 +1,38 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class BlogPage {
+
+    WebDriver driver;
+
+    public BlogPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Locators
+    private final By fullName_field = By.id("fullname");
+    private final By email_field = By.id("email");
+    private final By subscribe_button = By.id("_form_5_submit");
+    private final By thankYou_message = By.cssSelector("div._form-thank-you");
+
+
+    public void navigateToBlogPage() {
+        driver.get("https://www.blink22.com/blog/");
+    }
+
+    public void fillForm(String fullName, String email) {
+        driver.findElement(fullName_field).sendKeys(fullName);
+        driver.findElement(email_field).sendKeys(email);
+    }
+
+    public void clickSubscribeButton() {
+        driver.findElement(subscribe_button).click();
+    }
+
+    public void getThankYouMessage() {
+        driver.findElement(thankYou_message).getText();
+    }
+
 }
